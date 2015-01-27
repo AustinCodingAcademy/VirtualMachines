5ff512fa47042ce5cc58b3c1a9414e7c0eaf3703
Samir Patel <sameg14@gmail.com>
2015-01-27 13:13:57 -0600

    Added provisioner

---
+++ provision-aca-vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#original script context
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Provision machine
+cd "$DIR"
+:
+cd ../vms/aca.vm
+:
+
+# Bring up the VM, provisioning will happen here as well
+vagrant up;
+
+# SSH into VM
+vagrant ssh
